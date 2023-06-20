a6dd1acb6624c546910f56b914db375e2dc2d9e7
tugi <blueqwinn@gmail.com>
2023-06-20 10:40:00 +0300

    add bird.rb

---
+++ app/models/bird.rb
@@ -0,0 +1,20 @@
+class Bird < ApplicationRecord
+# GET/birds
+def index
+    birds = Bird.all 
+    render json: birds
+
+end
+
+# GET /birds/:id
+   
+
+
+def show
+
+    bird = bird.find_by(id: params[:id])
+
+    if bird
+       render json: bird
+    else
+        render json:
